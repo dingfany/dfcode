4764e4b91db623fd2cf679161a4eb6467f893d07
dingfany <dingfang.yang@nokia-sbell.com>
2020-05-20 16:01:57 +0800

    remove some print

--- python/pdfpro/pdfpro.py
+++ python/pdfpro/pdfpro.py
@@ -91,14 +91,7 @@ def mergePdf(inFileList, outFile):
 def test_split():
     myreport = 'data/MyFeeReport.pdf'
     splited='data/aftersplit.pdf'
-    # merged='data/aftermerge.pdf'
-
-
-    # filebasicinfo(myreport)
     splitPdf(myreport, splited,5,10)
-
-    # mergelist = ['data/hetong1.pdf','data/hetong2.pdf',splited]
-    # mergePdf(mergelist,merged)
 
 
 if __name__ == '__main__':
@@ -110,29 +103,32 @@ if __name__ == '__main__':
             print("-m  --merge merges the files")
             print("-s --split {startpage:endpage} split the file from start to end")        
             sys.exit()
+
         if opt_name in ('-v','--version'):
             print("[*] pdf split/merge pro Version is 0.9, Yangdingfang cook")
             sys.exit()
+
         if opt_name in ('-m','--merge'):
             print('merge files',args)
             merged='data/aftermerge.pdf'
             print('wring to data/aftermerge.pdf')
             mergePdf(args,merged)
             sys.exit()
+
         if opt_name in ('-s','--split'):
             print('split files to data/aftersplit.pdf ',args)
             page=opt_value
             page_range=page.split(':')
-            print(page_range[0], page_range[1])
             splited='data/aftersplit.pdf'       
-        # splitPdf(myreport, splited,5,10)
-            #args is list.
+            #args is list.convert to string.
             infile=args[0]  
             splitPdf(infile, splited, int(page_range[0]), int(page_range[1]))
             sys.exit()    
+
         if opt_name in ('-t'):
             test_split()
             sys.exit()
+
         print('complete')
         
 
